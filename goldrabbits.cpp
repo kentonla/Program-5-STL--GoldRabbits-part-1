6c05a9e3eac08a4ac7e150e8927b8bfc26eec4d1
kentonla <116829705+kentonla@users.noreply.github.com>
2024-11-11 00:58:13 -0800

    Base code

---
+++ goldrabbits.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+using namespace std;
+long long callsMade = 0;  //keeps track of how many times goldRabbits is called
+
+int goldRabbits(int n){
+    if (n==0 || n==1){
+        return 1;
+    }
+    else{
+        return goldRabbits(n-1) + goldRabbits(n-2);
+    }
+}
+
+int main()
+{
+    int const months = 12 * 10; // this is 10 years or 120 months
+    int start = time(0); // number of seconds since Jan 1, 1970
+    for(int i=0; i<months; i++)
+    {
+        int current = time(0); // number of seconds since program started
+        cout << setw(5)<<current-start<<":"; // print elapsed seconds
+        cout << " GoldRabbits("<<setw(2)<<i<<") = ";
+        cout << setw(11)<< goldRabbits(i) <<endl;// the call to goldRabbits
+    }
+}
