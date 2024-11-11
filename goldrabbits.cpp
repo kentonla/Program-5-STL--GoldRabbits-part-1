f576be0394c8bdcba03308946ab639fb00540328
kentonla <116829705+kentonla@users.noreply.github.com>
2024-11-11 01:18:26 -0800

    Added try/catch into goldRabbits function

--- goldrabbits.cpp
+++ goldrabbits.cpp
@@ -5,11 +5,20 @@ using namespace std;
 long long callsMade = 0;  //keeps track of how many times goldRabbits is called
 
 int goldRabbits(int n){
-    if (n==0 || n==1){
-        return 1;
+    try{
+        callsMade++;
+        if (n==0 || n==1){
+            return 1;
+        }
+        else if(n < 0){
+            throw callsMade;
+        }
+        else{
+            return goldRabbits(n-1) + goldRabbits(n-2);
+        }
     }
-    else{
-        return goldRabbits(n-1) + goldRabbits(n-2);
+    catch (int num){
+        cout << Overflow at fibo(num) << endl;
     }
 }
 
